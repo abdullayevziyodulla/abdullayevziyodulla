deff4f78cf145eb8b55d807f82da7c99d07547d0
Ziyodulla <zabdullayev@students.wiut.uz>
2025-02-20 07:40:02 -0500

    Add files via upload

---
+++ Portfolio Template 25.R
@@ -0,0 +1,140 @@
+#------------------------ WESTMINSTER INTERNATIONAL UNIVERSITY IN TASHKENT -----------------------#
+#-------------------------- INTRODUCTION TO STATISTICS AND DATA SCIENCE --------------------------#
+#------------------------------- PORTFOLIO OF INDIVIDUAL EXERCISES -------------------------------#
+#-----------------------------------ACADEMIC YEAR: 2024 - 2025 -----------------------------------#
+
+
+ID <-  NULL  # TO DO: <--- Insert your ID in place of NULL. 
+
+
+#--------------------------------------PACKAGE INSTALLATION AND LOADING PART-----------------------# 
+# Skip this part if you are only using the Base packages of R.
+# If you use any package other than the base package, 
+# please add the installation and loading codes here, as shown below (without #):
+# install.packages("package_name")
+# library(package_name)
+
+
+
+
+
+
+
+#-------------------------------------END OF PACKAGE INSTALLATIONS AND LOADING---------------------#
+
+# If your codes require several lines, you can add new lines above the answer variables.
+# But make sure to insert the final answers into the designated variable names by replacing NULL's.
+# Do not change the variable names, only these names will be used in grading.
+
+
+#------ TASK 1 -------# 
+
+task_1a <- NULL 
+
+
+task_1b <- NULL 
+
+
+#------ TASK 2 -------# 
+
+task_2a <- NULL 
+
+
+task_2b <- NULL 
+
+
+#------ TASK 3 -------# 
+
+task_3a <- NULL 
+
+
+task_3b <- NULL 
+
+
+#------ TASK 4 -------# 
+
+task_4a <- NULL 
+
+
+task_4b <- NULL 
+
+
+#------ TASK 5 -------# 
+
+task_5a <- NULL 
+
+
+task_5b <- NULL 
+
+
+task_5c <- NULL 
+
+
+task_5d <- NULL
+
+
+task_5e <- NULL 
+
+
+task_5f <- NULL 
+
+
+task_5g <- NULL 
+
+
+task_5h <- NULL
+
+
+task_5i <- NULL 
+
+
+task_5j <- NULL
+
+
+#------ TASK 6 -------# 
+
+task_6 <- NULL 
+
+
+#------ TASK 7 -------# 
+
+task_7 <- NULL 
+
+
+
+#------ TASK 8 -------# 
+
+# Import the dataset using the url link only: "https://s3.amazonaws.com/itao-30230/college.csv"
+college <- NULL
+
+# Delete the rows from the titanic dataset all at once according to your ID values:
+my_data <- NULL
+
+
+task_8a <- NULL 
+
+task_8b <- NULL 
+
+task_8c <- NULL 
+
+task_8d <- NULL
+
+
+# task_8e. Your codes below should plot the boxplots as requested.
+
+
+
+
+
+# task_8f. Your codes below should plot the histograms as requested.
+
+
+
+
+
+
+
+
+
+
+
